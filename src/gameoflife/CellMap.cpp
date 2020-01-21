a16972a5a93318b2961d42129624d4010ebec7b2
Asperatus <zersssin@gmail.com>
2020-01-21 10:22:35 +0100

    Modifying the naive version to use less memory

--- src/gameoflife/CellMap.cpp
+++ src/gameoflife/CellMap.cpp
@@ -1,83 +1,41 @@
 #include "CellMap.h"
 
 CellMap::CellMap(Game* g, unsigned int w, unsigned int h)
-	: game(g) width(w), height(h), length_in_bytes(w * h)
+	: game(g), width(w), height(h), length_in_bytes(w * h)
 {
 	cells = new Cell[length_in_bytes];
-	temp_cells = new Cell[length_in_bytes];
 	memset(cells, 0, length_in_bytes);
 }
 
 CellMap::~CellMap()
 {
 	delete[] cells;
-	delete[] temp_cells;
 }
 
-void CellMap::giveLifeToCell(unsigned int x, unsigned int y)
+void CellMap::changeCellState(unsigned int x, unsigned int y, int toAlive)
 {
-	int w = width, h = height;
+	unsigned int w = width, h = height;
 	Cell* cell_ptr = cells + (y * w) + x;
 
-	*(cell_ptr) |= 0x01;
-
-	if (x > 0 && y > 0)
-		*(cell_ptr - w - 1) += 0x02; // top left
-	if (y > 0)
-		*(cell_ptr - w) += 0x02; // top
-	if (x < w - 1 && y > 0)
-		*(cell_ptr - w + 1) += 0x02; // top right
-	if (x > 0)
-		*(cell_ptr - 1) += 0x02; // left
-	if (x < w - 1)
-		*(cell_ptr + 1) += 0x02; // right
-	if (x > 0 && y < h - 1)
-		*(cell_ptr + w - 1) += 0x02; // below left
-	if (y < h - 1)
-		*(cell_ptr + w) += 0x02; // below
-	if (x < w - 1 && y < h - 1)
-		*(cell_ptr + w + 1) += 0x02; // below right
+	if (toAlive)
+		*(cell_ptr) |= 0b01;
+	else
+		*(cell_ptr) &= ~0b01;
 }
 
-void CellMap::killCell(unsigned int x, unsigned int y)
+inline int CellMap::isAlive(unsigned int x, unsigned int y)
 {
-	int w = width, h = height;
-	Cell* cell_ptr = cells + (y * w) + x;
-
-	*(cell_ptr) &= ~0x01;
-
-	if (x > 0 && y > 0)
-		*(cell_ptr - w - 1) -= 0x02; // top left
-	if (y > 0)
-		*(cell_ptr - w) -= 0x02; // top
-	if (x < w - 1 && y > 0)
-		*(cell_ptr - w + 1) -= 0x02; // top right
-	if (x > 0)
-		*(cell_ptr - 1) -= 0x02; // left
-	if (x < w - 1)
-		*(cell_ptr + 1) -= 0x02; // right
-	if (x > 0 && y < h - 1)
-		*(cell_ptr + w - 1) -= 0x02; // below left
-	if (y < h - 1)
-		*(cell_ptr + w) -= 0x02; // below
-	if (x < w - 1 && y < h - 1)
-		*(cell_ptr + w + 1) -= 0x02; // below right
-}
-
-inline int CellMap::isAlive(int x, int y)
-{
-	return *(cells + (y * width) + x) & 0x01;
+	return *(cells + (y * width) + x) & 0b01;
 }
 
 void CellMap::nextGeneration()
 {
-	unsigned int count;
+	updateNeighbourCount();
+
 	unsigned int w = width, h = height;
 	Cell* cell_ptr;
 
-	memcpy(temp_cells, cells, length_in_bytes);
-
-	cell_ptr = temp_cells;
+	cell_ptr = cells;
 	for (unsigned int y = 0; y < h; ++y)
 	{
 		for (unsigned int x = 0; x < w; ++x)
@@ -85,12 +43,12 @@ void CellMap::nextGeneration()
 			if (*cell_ptr != 0)
 			{
 				neighbours = *cell_ptr >> 1;
-				if (*cell_ptr & 0x01)
+				if (*cell_ptr & 0b01)
 				{
 					// TODO: adapt the game's rules
 					if ((neighbours != 2) && (neighbours != 3))
 					{
-						killCell(x, y);
+						changeCellState(x, y, 0);
 						game->drawCell(x, y, 0);
 					}
 				}
@@ -99,16 +57,54 @@ void CellMap::nextGeneration()
 					// TODO: adapt the game's rules
 					if (neighbours == 3)
 					{
-						giveLifeToCell(x, y);
+						changeCellState(x, y, 1);
 						game->drawCell(x, y, 1);
 					}
 				}
 			}
-			cell_ptr++;
 		}
 	}
 }
 
-void CellMap::init()
+void CellMap::updateNeighbourCount()
+{
+	Cell* cell_ptr;
+
+	// we clear previous neighbour calculations
+	cell_ptr = cells;
+	for (unsigned int i = 0; i < length_in_bytes; ++i)
+	{
+		*(cell_ptr) &= 0b01;
+		++cell_ptr;
+	}
+
+	// and we calculate the neighbour count
+	cell_ptr = cells;
+	for (unsigned int y = 0; y < h; ++y)
+	{
+		for (unsigned int x = 0; x < w; ++w)
+		{
+			if (x > 0 && y > 0)
+				*(cell_ptr - w - 1) += 0b10; // top left
+			if (y > 0)
+				*(cell_ptr - w) += 0b10; // top
+			if (x < w - 1 && y > 0)
+				*(cell_ptr - w + 1) += 0b10; // top right
+			if (x > 0)
+				*(cell_ptr - 1) += 0b10; // left
+			if (x < w - 1)
+				*(cell_ptr + 1) += 0b10; // right
+			if (x > 0 && y < h - 1)
+				*(cell_ptr + w - 1) += 0b10; // below left
+			if (y < h - 1)
+				*(cell_ptr + w) += 0b10; // below
+			if (x < w - 1 && y < h - 1)
+				*(cell_ptr + w + 1) += 0b10; // below right
+			++cell_ptr;
+		}
+	}
+}
+
+void CellMap::loadRLE()
 {
 }
