48ab40bd4683e98027fab57fdf42f7d65ede26e9
palsor <tr.delapierre@gmail.com>
2020-01-22 22:08:29 +0100

    Create a basic QOpenGLWidget

--- src/ui/RenderArea.h
+++ src/ui/RenderArea.h
@@ -1,24 +1,34 @@
 #ifndef UI_RENDERAREA_H_
 #define UI_RENDERAREA_H_
 
-#include <QBrush>
-#include <QPen>
-#include <QPixmap>
-#include <QWidget>
 #include "../logic/Universe.h"
+#include "../gameoflife/Game.h"
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QWheelEvent>
 
-class RenderArea : public QWidget {
+class RenderArea : public QOpenGLWidget , protected QOpenGLFunctions {
 
 public:
 	RenderArea(QWidget *parent = 0, Universe *universe = 0);
-	virtual ~RenderArea();
 
 protected:
-	void paintEvent(QPaintEvent *event) override;
+	void initializeGL() override;
+	void resizeGL(int w, int h) override;
+	void paintGL() override;
+	void wheelEvent(QWheelEvent *event) override;
 
 private:
 	Universe *universe;
+	std::string vertexShaderSource;
+	std::string fragmentShaderSource;
 
+	GLuint m_posAttr;
+	GLuint m_colAttr;
+	GLuint m_matrixUniform;
+
+	QOpenGLShaderProgram *m_program;
 };
 
 #endif /* UI_RENDERAREA_H_ */
