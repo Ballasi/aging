91adc205efc2a507139f4a305ca2923119bdd98b
DocDoomDuck <docdoomduck@protonmail.com>
2020-02-14 17:44:17 +0100

    Removing pile of cout & saving result for mini step

--- src/logic/Universe.cpp
+++ src/logic/Universe.cpp
@@ -315,34 +315,28 @@ Quadrant *Universe::result(size_t level, MacroCell* macrocell_tmp) {
   if (macrocell_tmp->result != nullptr) return macrocell_tmp->result;
 
   if (level <= 2) {
-    cout << "0" << endl;
     ( (Quadrant*)macrocell_tmp )->debug(level);
     int nb_nw =  macrocell_tmp->nw->minicell.nw + macrocell_tmp->nw->minicell.ne + macrocell_tmp->ne->minicell.nw  +
                  macrocell_tmp->nw->minicell.sw +                                  macrocell_tmp->ne->minicell.sw  +
                  macrocell_tmp->sw->minicell.nw + macrocell_tmp->sw->minicell.ne + macrocell_tmp->se->minicell.nw;
 
-    cout << "1" << endl;
     int nb_ne =  macrocell_tmp->nw->minicell.ne + macrocell_tmp->ne->minicell.nw + macrocell_tmp->ne->minicell.ne  +
                  macrocell_tmp->nw->minicell.se +                                  macrocell_tmp->ne->minicell.se  +
                  macrocell_tmp->sw->minicell.ne + macrocell_tmp->se->minicell.nw + macrocell_tmp->se->minicell.ne;
 
-    cout << "2" << endl;
     int nb_sw =  macrocell_tmp->nw->minicell.sw + macrocell_tmp->nw->minicell.se + macrocell_tmp->ne->minicell.sw  +
                  macrocell_tmp->sw->minicell.nw +                                  macrocell_tmp->se->minicell.nw  +
                  macrocell_tmp->sw->minicell.sw + macrocell_tmp->sw->minicell.se + macrocell_tmp->se->minicell.sw;
 
-    cout << "3" << endl;
     int nb_se =  macrocell_tmp->nw->minicell.se + macrocell_tmp->ne->minicell.sw + macrocell_tmp->ne->minicell.se  +
                  macrocell_tmp->sw->minicell.ne +                                  macrocell_tmp->se->minicell.ne  +
                  macrocell_tmp->sw->minicell.se + macrocell_tmp->se->minicell.sw + macrocell_tmp->se->minicell.se;
 
-    cout << "4" << endl;
     CellState new_nw ;
     CellState new_ne ;
     CellState new_sw ;
     CellState new_se ;
     // I check if the cell is alive
-    cout << "5" << endl;
     if (macrocell_tmp->nw->minicell.se) {
       // If the cell is alive, i check if cell survive
       if ((nb_nw == 2) || (nb_nw == 3)) {
@@ -392,8 +386,8 @@ Quadrant *Universe::result(size_t level, MacroCell* macrocell_tmp) {
         else { new_se = 0; }
     }
 
-    cout << "6" << endl;
-    return (Quadrant *)minicell(new_nw, new_ne, new_se, new_sw);
+    macrocell_tmp->result = (Quadrant *)minicell(new_nw, new_ne, new_se, new_sw);
+    return macrocell_tmp->result;
   } else {
 
     // voir figure 4 de : https://www.drdobbs.com/jvm/an-algorithm-for-compressing-space-and-t/184406478
@@ -401,71 +395,56 @@ Quadrant *Universe::result(size_t level, MacroCell* macrocell_tmp) {
     // a partir de ma macroCell de taille n,
     // je vais faire 6 assemblages différents de macroCell de taille n-2
     // afin de pouvoir avoir les 4 carrés centraux (de taille n-2), puis reconstruire la cellules centrale (n-1).
-    cout << "7" << endl;
     Quadrant *temp_nw = result(level - 1, (MacroCell *)macrocell_tmp->nw);
 
-    cout << "8" << endl;
     Quadrant *temp_n =
         result(level - 1, macrocell(level - 1, macrocell_tmp->nw->macrocell.ne,
                                     macrocell_tmp->ne->macrocell.nw,
                                     macrocell_tmp->nw->macrocell.se,
                                     macrocell_tmp->ne->macrocell.sw));
 
-    cout << "9" << endl;
     Quadrant *temp_ne = result(level - 1, (MacroCell *)macrocell_tmp->ne);
 
-    cout << "10" << endl;
     Quadrant *temp_w =
         result(level - 1, macrocell(level - 1, macrocell_tmp->nw->macrocell.sw,
                                     macrocell_tmp->nw->macrocell.se,
                                     macrocell_tmp->sw->macrocell.nw,
                                     macrocell_tmp->sw->macrocell.ne));
 
-    cout << "11" << endl;
     Quadrant *temp_c =
         result(level - 1, macrocell(level - 1, macrocell_tmp->nw->macrocell.se,
                                     macrocell_tmp->ne->macrocell.sw,
                                     macrocell_tmp->sw->macrocell.ne,
                                     macrocell_tmp->se->macrocell.nw));
 
-    cout << "12" << endl;
     Quadrant *temp_e =
         result(level - 1, macrocell(level - 1, macrocell_tmp->ne->macrocell.sw,
                                     macrocell_tmp->ne->macrocell.se,
                                     macrocell_tmp->se->macrocell.nw,
                                     macrocell_tmp->se->macrocell.ne));
 
-    cout << "13" << endl;
     Quadrant *temp_sw = result(level - 1, (MacroCell *)macrocell_tmp->sw);
 
-    cout << "14" << endl;
     Quadrant *temp_s =
         result(level - 1, macrocell(level - 1, macrocell_tmp->sw->macrocell.ne,
                                     macrocell_tmp->se->macrocell.nw,
                                     macrocell_tmp->sw->macrocell.se,
                                     macrocell_tmp->se->macrocell.sw));
 
-    cout << "15" << endl;
     Quadrant *temp_se = result(level - 1, (MacroCell *)macrocell_tmp->se);
 
-    cout << "16" << endl;
     Quadrant *res_nw = result(
         level - 1, macrocell(level - 1, temp_nw, temp_n, temp_w, temp_c));
-    cout << "17" << endl;
     Quadrant *res_ne = result(
         level - 1, macrocell(level - 1, temp_n, temp_ne, temp_c, temp_e));
-    cout << "18" << endl;
     Quadrant *res_sw = result(
         level - 1, macrocell(level - 1, temp_w, temp_c, temp_sw, temp_s));
-    cout << "19" << endl;
     Quadrant *res_se = result(
         level - 1, macrocell(level - 1, temp_c, temp_e, temp_s, temp_se));
 
-    cout << "20" << endl;
     macrocell_tmp->result =
         (Quadrant *)macrocell(level - 1, res_nw, res_ne, res_sw, res_se);
 
-    cout << "21" << endl;
     return macrocell_tmp->result;
   }
 }
