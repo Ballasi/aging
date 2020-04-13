bee2abb622455a52ba2a73ba49566d5270e69bfa
DocDoomDuck <docdoomduck@protonmail.com>
2020-04-13 15:11:09 +0200

    New Rules

--- src/model/Rule.cpp
+++ src/model/Rule.cpp
@@ -2,30 +2,127 @@
 #include <cstddef>
 #include <cstring>
 #include <model/Rule.h>
+#include <stdexcept>
 
-Rule::Rule(size_t state_count, size_t max_neighbour_count)
-    : state_count(state_count), max_neighbour_count(max_neighbour_count) {
-  lookup_table = new CellState[state_count * max_neighbour_count]();
-
-  shift = 0;
-  while (max_neighbour_count >>= 1)
-    shift++;
+/*
+  Returns the number of bits required to write n.
+ */
+inline size_t required_bits(size_t n) {
+  int result = 0;
+  while (n >>= 1)
+    result++;
+  return result;
 }
 
-Rule::~Rule() { delete lookup_table; }
+Rule::Rule(QString rule_string)
+  : _state_count(2), _neighbour_count(9), _row_length(2),
+    _transition_table(new CellState[2 * 9]()) {
+  enum ReadingState { B_SEARCH, B_VALUES, S_SEARCH, S_VALUES };
 
-void Rule::clear(CellState state) {
-  for (CellState state = 0; state < state_count; ++state) {
-    for (size_t neighbour_count = 0; neighbour_count <= max_neighbour_count;
-         ++neighbour_count)
-      set(state, neighbour_count, state);
+  ReadingState reading_state = B_SEARCH;
+  for (QChar c: rule_string) {
+    switch (reading_state) {
+    case B_SEARCH:
+      if (c == 'B')
+        reading_state = B_VALUES;
+      else
+        throw std::invalid_argument("rule_string should start with B");
+      break;
+
+    case B_VALUES:
+      if ('0' <= c && c <= '8') {
+        size_t neighbour = c.toLatin1() - '0';
+        set(neighbour, 0, 1);
+      } else if (c == '/') {
+        reading_state = S_SEARCH;
+      } else {
+        throw std::invalid_argument("Birth values should be between 0 and 8");
+      }
+      break;
+
+    case S_SEARCH:
+      if (c == 'S')
+        reading_state = S_VALUES;
+      else
+        throw std::invalid_argument("rule_string should have S after \'/\'");
+      break;
+
+    case S_VALUES:
+      if ('0' <= c && c <= '8') {
+        size_t neighbour = c.toLatin1() - '0';
+        set(neighbour, 1, 1);
+      } else {
+        throw std::invalid_argument("Survival values should be between 0 and 8");
+      }
+      break;
+    }
+  }
+  if (reading_state != S_VALUES)
+    throw std::invalid_argument("Incomplete rule string");
+}
+
+Rule::Rule(size_t state_count, size_t neighbour_count)
+    : _state_count(state_count), _neighbour_count(neighbour_count),
+      _row_length(required_bits(neighbour_count)) {
+  _transition_table = new CellState[_row_length * neighbour_count]();
+}
+
+Rule::~Rule() { delete[] _transition_table; }
+
+
+// Rule usage
+
+inline CellState Rule::apply(CellState state, size_t neighbour_count) const {
+  return _transition_table[neighbour_count + (state << _row_length)];
+}
+
+
+// Rule editing
+
+inline void Rule::set(CellState state, size_t neighbour, CellState result) {
+  _transition_table[neighbour + (state << _row_length)] = result;
+}
+
+inline void Rule::clear(CellState state) {
+  for (CellState state = 0; state < _state_count; ++state) {
+    for (size_t neighbour = 0; neighbour <= _neighbour_count; ++neighbour)
+      set(state, neighbour, state);
   }
 }
 
-void Rule::set(CellState state, size_t neighbour_count, CellState result) {
-  lookup_table[neighbour_count + (state << shift)] = result;
+
+// Getters
+
+inline size_t Rule::state_count() { return _state_count; }
+
+inline size_t Rule::neighbour_count() { return _neighbour_count; }
+
+QString Rule::to_rule_string() {
+  // Exceptions
+  if (_state_count != 2) {
+    throw std::domain_error("Rule is not for 2 cellular automaton.");
+  }
+  if (_neighbour_count > 9) {
+    throw std::domain_error("Rule uses neighbourhoods bigger than 9.");
+  }
+
+  QString result;
+  // Birth
+  result.append("B");
+  for (size_t neighbour = 0; neighbour < _neighbour_count; ++neighbour) {
+    if (apply(0, neighbour)) {
+      result.push_back(std::to_string(neighbour).c_str());
+    }
+  }
+
+  // Survival
+  result.append("/S");
+  for (size_t neighbour = 0; neighbour < _neighbour_count; ++neighbour) {
+    if (apply(1, neighbour)) {
+      result.push_back(std::to_string(neighbour).c_str());
+    }
+  }
+
+  return result;
 }
 
-CellState Rule::apply(CellState state, size_t neighbour_count) const {
-  return lookup_table[neighbour_count + (state << shift)];
-}
--- src/model/Rule.h
+++ src/model/Rule.h
@@ -3,23 +3,93 @@
 
 #include "CellState.h"
 #include <cstddef>
+#include <QString>
 
+/*
+  Describes a classical rule for a simple cellular automaton,
+  where only initial state and the number of neighbour is used.
+*/
 class Rule {
 public:
-  Rule(size_t state_count, size_t max_neighbour_count);
+  /*
+    Constructs a rule from a rule string in the B/S format.
+    Other formats are not yet supported.
+
+    Exception:
+    If `rule_string` is invalid an std::invalid_argument is thrown.
+  */
+  explicit Rule(QString rule_string);
+
+  /*
+    Creates a "blank" rule where every situation leads to
+    the default cell state.
+  */
+  Rule(size_t state_count, size_t neighbour_count);
+
+  /*
+    Destructor
+  */
   ~Rule();
 
+  /*
+    Returns the new state of a cell of state `state` with `neighbour` neighbours
+    obtained when the rule is applied.
+  */
+  CellState apply(CellState state, size_t neighbour) const;
+
+  /*
+    Sets the result of the rule when applied to a cell
+    of state `state` and with `neighbour` neighbours.
+  */
+  void set(CellState state, size_t neighbour, CellState result);
+
+  /*
+    Clears the rule so that every situation leads to the default state.
+   */
   void clear(CellState state = 0);
-  void set(CellState state, size_t neighbour_count, CellState result);
-  CellState apply(CellState state, size_t neighbour_count) const;
+
+  /*
+    Returns the number of states handled by the rule.
+  */
+  size_t state_count();
+
+  /*
+    Returns the number of neighbour count handled by the rule.
+  */
+  size_t neighbour_count();
+
+  /*
+    Returns a the rule string using the B/S rule format.
+
+    Exception:
+    If this rule is not about a 2 state automaton or
+    uses a neighbourhood of more than 9 neighbours,
+    a std::domain_error is raised.
+  */
+  QString to_rule_string();
 
 private:
-  size_t state_count;
-  size_t max_neighbour_count;
+  /*
+    Maximum state value handled by the rule.
+  */
+  size_t _state_count;
 
-  size_t shift;
+  /*
+    Maximum number of neighbour handled by the rule.
+  */
+  size_t _neighbour_count;
+  /*
+    Holdes the transition from every state of with every neighbour count.
+    Each row describes state,
+    each collumn a neighbour count (aka a serounding).
+  */
+  CellState *_transition_table;
 
-  CellState *lookup_table;
+  /*
+    Length of `transition_table` row's.
+    Is the first power of two bigger or equal to neighbour_count
+  */
+  size_t _row_length;
 };
 
 #endif // RULE_HPP
