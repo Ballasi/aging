7f74d6b7e1ec5c5585bd3392864b18823ad57144
DocDoomDuck <docdoomduck@protonmail.com>
2020-05-22 18:17:39 +0200

    Documenting Cell state

--- src/model/CellState.h
+++ src/model/CellState.h
@@ -1,8 +1,17 @@
 #ifndef CELL_STATE_H_
 #define CELL_STATE_H_
 
+/*!
+ * \file CellState.h
+ */
+
 #include <cstddef>
 
+/*!
+ * \brief Represents a cell state
+ *
+ * All values must positive integers.
+ */
 typedef size_t CellState;
 
 #endif // CELL_STATE_H_
