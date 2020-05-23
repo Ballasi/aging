24f4fa7f29b80bf832c4cb13c5f9238744c72f3c
DocDoomDuck <docdoomduck@protonmail.com>
2020-05-23 17:36:41 +0200

    Documenting Logger

--- src/logs/Logger.h
+++ src/logs/Logger.h
@@ -1,18 +1,46 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+/*!
+ * \file Logger.h
+ * \brief File logging utilities
+ */
+
 #include <QObject>
 #include <QPlainTextEdit>
 #include <QFile>
 #include <QTextStream>
 #include <QDateTime>
 
+/*!
+ * \class Logger
+ * \brief Main logger class
+ */
 class Logger : public QObject {
     Q_OBJECT
 
 public:
+  /*!
+   * \brief Constructor
+   *
+   * \param parent: Parent for Qt interaction 
+   * \param fileName: name of file to log to
+   * \param append: appends to file
+   */
   explicit Logger(QObject *parent, QString fileName, bool append);
+
+  /*!
+   * \brief Destructor
+   *
+   * Closes logging file
+   */
   ~Logger();
+
+  /*!
+   * \brief Toggle date in log
+   *
+   * Defaults to true
+   */
   void setShowDateTime(bool value);
 
 private:
@@ -20,6 +48,9 @@ private:
   bool m_showDate;
 
 public slots:
+  /*!
+   * \brief writes to the log file
+   */
   void write(const QString &value);
 };
 
