b5b59bf38bdb30883ed4a41c62b41eff4f4bd6cd
DocDoomDuck <docdoomduck@protonmail.com>
2020-05-23 18:13:56 +0200

    Documenting MiniCell

--- src/universes/hash/cell/MiniCell.h
+++ src/universes/hash/cell/MiniCell.h
@@ -1,18 +1,52 @@
 #ifndef MINICELL_HPP
 #define MINICELL_HPP
 
-#include <functional>
+/*!
+ * \file MiniCell.h
+ * \brief HashUniverse's MiniCell
+ */
 
+#include <functional>
 #include <model/CellState.h>
 
 namespace cell {
 union Quadrant;
+/*!
+ * \class MiniCell
+ * \brief A 2x2 cell grid
+ */
 class MiniCell {
 public:
+  /*!
+   * \brief Default constructor
+   */
   MiniCell() = default;
+
+  /*!
+   * \brief Constructor
+   *
+   * \param nw: state of north-west
+   * \param ne: state of north-east
+   * \param sw: state of south-west
+   * \param se: state of south-east
+   */
   MiniCell(CellState nw, CellState ne, CellState sw, CellState se);
 
+  /*!
+   * \brief Equality operator
+   *
+   * MiniCells are equal if they have the
+   * exact same sub cell states.
+   *
+   * \param other: MiniCell to compare with
+   */
   bool operator==(const MiniCell other) const;
+
+  /*!
+   * \brief Handy indexed getter
+   *
+   * \param index: index of the cell state
+   */
   CellState operator[](const size_t index) const;
 
   friend std::hash<MiniCell>;
@@ -25,15 +59,27 @@ public:
 
 namespace std {
 using cell::MiniCell;
+/*!
+ * \struct MiniCell hasher
+ */
 template <> struct hash<MiniCell> {
+  /*!
+   * \brief Constructor
+   */
   hash() = default;
 
+  /*!
+   * \brief Call operator
+   *
+   * \param minicell: minicell to hash
+   */
   size_t operator()(const MiniCell &minicell) const {
-    return hasher(minicell.nw) ^ hasher(minicell.ne) ^ hasher(minicell.sw) ^
-           hasher(minicell.se);
+    return hasher(minicell.nw) ^ hasher(minicell.ne)
+         ^ hasher(minicell.sw) ^ hasher(minicell.se);
   }
 
   hash<CellState> hasher;
 };
 } // namespace std
+
 #endif // MINICELL_HPP
